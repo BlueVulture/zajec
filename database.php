a80fad1cdeb3343e4526f1a3335bafc1ac51577d
RattleTotem <domen.ramsak@gmail.com>
2017-05-04 13:00:01 +0200

    test

--- database.php
+++ database.php
@@ -6,7 +6,7 @@ $db_name = 'zajec';
 $db_server = 'localhost';
 
 //povezava na mysql strežnik
-mysql_connect($db_server, $db_user, $db_pass);
+mysqli_connect($db_server, $db_user, $db_pass);
 
 //izbira podatkovne baze na strežniku
 mysql_select_db($db_name);
