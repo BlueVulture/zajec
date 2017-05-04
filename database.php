ee825ca53253fed59f30b67b9127800f3752dfa4
RattleTotem <domen.ramsak@gmail.com>
2017-05-04 13:17:57 +0200

    komentarji

--- database.php
+++ database.php
@@ -5,13 +5,13 @@ $db_pass = 'abc123';
 $db_name = 'zajec';
 $db_server = 'localhost';
 
-//povezava na mysql strežniksdsd
+//povezava na mysql strežnik
 mysqli_connect($db_server, $db_user, $db_pass);
 
 //izbira podatkovne baze na strežniku
 mysql_select_db($db_name);
 
-//reševanje težave s šumnikiwerwer
+//reševanje težave s šumniki
 mysql_query("SET NAMES 'utf8'");
 
 ?>
