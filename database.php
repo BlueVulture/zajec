041e787e8bcc4f143b7d2dad486d85134e9b5901
RattleTotem <domen.ramsak@gmail.com>
2017-05-04 13:00:36 +0200

    Update database.php
    git update

--- database.php
+++ database.php
@@ -5,7 +5,7 @@ $db_pass = 'abc123';
 $db_name = 'zajec';
 $db_server = 'localhost';
 
-//povezava na mysql strežnik
+//povezava na mysql strežniksdsd
 mysqli_connect($db_server, $db_user, $db_pass);
 
 //izbira podatkovne baze na strežniku
