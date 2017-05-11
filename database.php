a39e91e392d30ac62e225092dc68b5c4a298de61
RattleTotem <domen.ramsak@gmail.com>
2017-05-11 10:44:17 +0200

    povezava na podatkovno bazo, pa mysqli

--- database.php
+++ database.php
@@ -1,17 +1,17 @@
 <?php
 
-$db_user = 'root';
-$db_pass = 'abc123';
-$db_name = 'zajec';
-$db_server = 'localhost';
+$db_user ='b31_20068105';
+$db_pass = 'BeliZajec123';
+$db_name = 'b31_20068105_zajec';
+$db_server = 'sql308.byetcluster.com';
 
 //povezava na mysql strežnik;
-mysql_connect($db_server, $db_user, $db_pass);
+$conn = mysqli_connect($db_server, $db_user, $db_pass);
 
 //izbira podatkovne baze na strežniku
-mysql_select_db($db_name);
+$db = mysqli_select_db($db_name);
 
 //reševanje težave s šumniki
-mysql_query("SET NAMES 'utf8'");
+$query = mysqli_query("SET NAMES 'utf8'");
 
 ?>
