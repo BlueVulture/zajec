d2cdeaaa6bc899cc17aed3ad8e1cbac4b727637c
Gregor <motherlode333@gmail.com>
2017-05-18 12:50:36 +0200

    asdasd

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 $db_user ='b31_20068105';
 $db_pass = 'BeliZajec123';
 $db_name = 'zajec';
-$db_server = 'localhost';
+$db_server = 'localhost:81';
 
 //povezava na mysql strežnik;
 $conn = mysqli_connect($db_server, $db_user, $db_pass);
@@ -13,5 +13,5 @@ $db = mysqli_select_db($conn, $db_name);
 
 //reševanje težave s šumniki
 $query = mysqli_query($conn, "SET NAMES 'utf8'");
-
+//test za git hub
 ?>
