ab16da69cd7ab234fe1b10616f12f8420ed7df60
RattleTotem <domen.ramsak@gmail.com>
2017-06-03 16:45:14 +0200

    slike

--- ad_view.php
+++ ad_view.php
@@ -32,7 +32,7 @@ $ad = mysqli_fetch_array($result);
             while ($picture = mysqli_fetch_array($result)) {
                 echo '<div class="delete_wrap">';
                 if ($_SESSION['user_id'] == $ad['user_id']) {
-                    echo '<a href="delete_picture.php?id='.$picture['id'].'&ad_id='.$ad_id.'" 
+                    echo '<a href="delete_picture.php?id='.$picture['id'].'&ad_id='.$ad_id.'"
                           class="myaction"
                           onclick="return confirm(\'Ali ste prepričani?\');">Izbriši</a>';
                     //echo '<br />';
@@ -41,7 +41,7 @@ $ad = mysqli_fetch_array($result);
                       <img src="' . $picture['url'] . '" alt="slika" width="120" />
                       </a>';
                 //echo '<br />';
-                
+
                 echo '</div>';
             }
         }
@@ -50,8 +50,15 @@ $ad = mysqli_fetch_array($result);
             if ($_SESSION['user_id'] == $ad['user_id']) {
         ?>
         <form action="ad_add_picture.php" method="post" enctype="multipart/form-data" id="nalozi-slike">
+
             <input type="hidden" name="id" value="<?php echo $ad_id; ?>" />
             <input type="file" name="file" />
+            <input type="submit" value="Naloži" />
+        </form>
+        <span>Naloži video:</span>
+        <form action="ad_add_picture.php" method="post" enctype="multipart/form-data" id="nalozi-slike">
+            <input type="hidden" name="id" value="<?php echo $ad_id; ?>" />
+            <input type="url" name="file" />
             <input type="submit" value="Naloži" />
         </form>
         <?php
@@ -63,14 +70,14 @@ $ad = mysqli_fetch_array($result);
         echo '<h3>' . $ad['title'] . '</h3>';
         echo '<i>' . $ad['name'] . '</i><br />';
         echo $ad['date_b'] . ' - ' . $ad['date_e'];
-        echo '<h3>' . $ad['price'] . '</h3>';
+        echo '<h4 id="cena">Cena: ' . $ad['price'] . ' €</h4>';
         echo '<p>' . $ad['description'] . '</p>';
 ?>
     </div>
     <div id="comments">
         <h4>Komentiraj:</h4>
         <form action="comments_insert.php" method="post">
-            <input type="hidden" name="ad_id" 
+            <input type="hidden" name="ad_id"
                    value="<?php echo $ad_id; ?>" />
             <textarea name="comment" cols="50" rows="4"></textarea>
             <br />
@@ -79,19 +86,19 @@ $ad = mysqli_fetch_array($result);
         <hr>
 <?php
    //izpis vseh komentarjev za ta oglas
-    $query = "SELECT c.*, u.first_name, u.last_name  
-              FROM comments c INNER JOIN users u 
+    $query = "SELECT c.*, u.first_name, u.last_name
+              FROM comments c INNER JOIN users u
               ON c.user_id = u.id
               WHERE c.ad_id = $ad_id
               ORDER BY c.date_c DESC";
     $result = mysqli_query($conn, $query);
-    
+
     while ($row = mysqli_fetch_array($result)) {
         echo '<div class="comment">';
         echo '<span class="com_aut">'.$row['first_name'].' '.$row['last_name'].' - '.date('d. m. Y H:i:s',strtotime($row['date_c'])).'</span>';
-        
+
         echo htmlspecialchars($row['content']);
-        
+
         if (($ad['user_id'] == $_SESSION['user_id']) || ($row['user_id'] == $_SESSION['user_id'] )) {
             echo '<br />';
             echo '<a href="comments_delete.php?id='.$row['id'].'&ad_id='.$ad_id.'">Izbriši</a>';
