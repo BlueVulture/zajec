33cbfd9ee8297a0ab148c28ac4dee8c04ae75f97
RattleTotem <domen.ramsak@gmail.com>
2017-06-03 16:20:38 +0200

    insert

--- ad_insert.php
+++ ad_insert.php
@@ -1,22 +1,22 @@
 <?php
     include_once 'session.php';
     include_once 'database.php';
-    
+
     $title = $_POST['title'];
     $date_b = date("Y-m-d h:m:s");
     $date_e = $_POST['date_e'];
     $price = $_POST['price'];
     $category_id = (int)$_POST['category_id'];
     $description = $_POST['description'];
-    
+
     $user_id = (int)$_SESSION['user_id'];
     //preverim, če so izpolnjeni obvezni atributi
-    if (!empty($title) && !empty($date_b) 
+    if (!empty($title) && !empty($date_b)
             && !empty($date_e) && !empty($price)
             ) {
-        $query = sprintf("INSERT INTO ads(title, 
-                           date_b, date_e, price, 
-                           category_id, user_id, 
+        $query = sprintf("INSERT INTO ads(title,
+                           date_b, date_e, price,
+                           category_id, user_id,
                            description)
                           VALUES ('%s','%s','%s',
                           '%s',$category_id,
@@ -28,9 +28,9 @@
                 mysqli_real_escape_string($conn, $description)
                 );
                 mysqli_query($conn, $query);
-                $ad_id = mysqli_insert_id();
-//                var_dump($ad_id);
-                header("Location: ad_view.php?id='$ad_id'");
+                $ad_id = mysqli_insert_id($conn);
+                //var_dump($ad_id);
+                header("Location: ad_view.php?id=$ad_id");
     }
     else {
         header("Location: ad_add.php");
